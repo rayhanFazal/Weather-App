6e5b2f5945a301a903b36a22ce4a7cd787e2f530
Rayhan <rayhanfaz7@gmail.com>
2021-09-07 20:56:24 -0400

    Completed get_weather_info() function

--- weather_app.py
+++ weather_app.py
@@ -1,5 +1,5 @@
 from __future__ import annotations
-
+from typing import Tuple
 import tkinter
 from tkinter import *
 from configparser import ConfigParser
@@ -18,6 +18,19 @@ def create_api_file() -> None:
     f.write("[" + key_name + "]" + "\n")
     key = input("Enter key:")
     f.write("api=" + key)
+
+def get_weather_info(location: str) -> tuple:
+    """Return a tuple containing the city, country, temperature, and weather
+    of <location>"""
+    info = requests.get(url.format(location, api))
+    j = info.json()
+    # retrieve necessary info from json file <j>
+    city = j["name"]
+    country = j["sys"]
+    temp = j["main"]["temp"] - 273.15 # convert kelvin to celsius
+    w = j["weather"][0]["main"]
+    return (city, country, temp, w)
+
 
 window = Tk()
 window.geometry("400x300")
@@ -40,6 +53,9 @@ create_api_file()
 c = ConfigParser()
 c.read("config.ini")
 
-api = c[key_name]['api']
+api = c[key_name]['api'] # retrieve api key
 url = "https://api.openweathermap.org/data/2.5/weather?q={}&appid={}"
+
+get_weather_info(i.get(1.0, "end-1c"))
+
 window.mainloop()
